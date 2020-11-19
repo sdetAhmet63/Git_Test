649284532108b841624862d66f1a86e88f203828
Sdet_Ahmet <parisstar033@gmail.com>
2020-11-19 19:40:26 +0100

    master branch 21:39

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,8 +1,14 @@
 public class TestClass {
-}
+
 
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
-         System.out.println("GitHub dan merhaba !");
+        System.out.println("GitHub dan merhaba !");
         System.out.println("branch ile calisma : 19 Kasim");
+
+
+        System.out.println("master branch 21:39");
+
+
+    }
     }
