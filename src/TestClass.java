1522b7849b5b5f3e627781a39d7f1a0cbed9e894
Sdet_Ahmet <parisstar033@gmail.com>
2020-11-19 19:45:14 +0100

    cnflict

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,12 @@
 public class TestClass {
-}
+
 
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
-         System.out.println("GitHub dan merhaba !");
+        System.out.println("GitHub dan merhaba !");
         System.out.println("branch ile calisma : 19 Kasim");
         System.out.println("branch ahmet ten selam");
+
+        System.out.println("conflict nasil cozulur");
     }
+}
