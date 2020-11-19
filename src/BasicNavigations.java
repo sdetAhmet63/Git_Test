2b24172e428518b8e531111573ac4da7b847ecb4
Sdet_Ahmet <parisstar033@gmail.com>
2020-11-19 19:31:53 +0100

    branch bir

---
+++ src/BasicNavigations.java
@@ -0,0 +1,2 @@
+public class BasicNavigations {
+}
--- src/TestClass.java
+++ src/TestClass.java
@@ -4,4 +4,5 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
          System.out.println("GitHub dan merhaba !");
+        System.out.println("branch ile calisma : 19 Kasim");
     }
